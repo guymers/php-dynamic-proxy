7e94d8279a6eaeab5dc5c19f3007894b9e5612f3
Sam Guymer <sam@guymer.me>
2017-09-05 20:31:20 +1000

    Only load test files when testing

--- src/test/resources/bootstrap.php
+++ src/test/resources/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 
 define("APPLICATION_PATH", __DIR__ . "/../../..");
-require(APPLICATION_PATH . "/vendor/autoload.php");
+$loader = require(APPLICATION_PATH . "/vendor/autoload.php");
+$loader->addPsr4("guymers\\proxy\\mock\\", __DIR__ . "/../php/guymers/proxy/mock");
